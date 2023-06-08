febbcc6dc26e19bd259c45071b50319105321317
ian.tech4me <ian940266@gmail.com.br>
2023-06-07 22:21:20 -0300

    Update App.java

--- aplicacao/App.java
+++ aplicacao/App.java
@@ -1,5 +1,12 @@
 package aplicacao;
 
+import model.Pizza;
+
 public class App {
-    
+    public static void main(String[] args) {
+        Pizza pedido = new Pizza("frango", 30, 'p');
+
+        System.out.println("Pedido criado!!!");
+        System.out.println(pedido.pedidoIfood());
+    }
 }
--- model/Pizza.java
+++ model/Pizza.java
@@ -1,5 +1,43 @@
 package model;
 
 public class Pizza{
+    private String sabor;
+    private double valor;
+    private char tamanho;
+
     
+
+    public Pizza(String sabor, double valor, char tamanho) {
+        this.sabor = sabor;
+        this.valor = valor;
+        this.tamanho = tamanho;
+    }
+
+    public String pedidoIfood(){
+        return String.format("sabor: %s, valor %.2f, tamanho %s", getSabor(), getValor(),getTamanho());
+    }
+
+    public void setSabor(String sabor){
+        this.sabor = sabor;
+    }
+
+    public String getSabor(){
+        return this.sabor;
+    }
+
+    public void setValor(Double valor){
+        this.valor = valor;
+    }
+
+    public double getValor(){
+        return valor;
+    }
+
+    public void setTamanho(char tamanho){
+        this.tamanho = tamanho;
+    }
+
+    public  char getTamanho(){
+        return tamanho;
+    }
 }
